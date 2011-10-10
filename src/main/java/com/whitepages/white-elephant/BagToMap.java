3814d2761afeef4e238b4b7514bc4c83f7321a90
Jay Hoover <jhoover@whitepages.com>
2011-10-10 11:00:36 -0700

    code review feedback for BagToMap.java

--- src/main/java/com/whitepages/white-elephant/BagToMap.java
+++ src/main/java/com/whitepages/white-elephant/BagToMap.java
@@ -16,26 +16,30 @@ import org.apache.pig.impl.logicalLayer.schema.Schema;
  * an input set of tuples
 */
 
-public class BagToMap extends EvalFunc<Map<Object,Object>> {
+public class BagToMap extends EvalFunc<Map<String,Object>> {
     /**
      * @return map of data from the Tuple input
      */
-    public Map<Object,Object> exec(Tuple input) throws IOException {
+    public Map<String,Object> exec(Tuple input) throws IOException {
         if (input == null || input.size() < 1 || input.get(0) == null)
             return null;
 
         try{
-            Map<Object, Object> mapData = new HashMap<Object, Object>();
+            Map<String, Object> mapData = new HashMap<String, Object>();
             
             DataBag keys = (DataBag)input.get(0);
             DataBag values = (DataBag)input.get(1);
 
+            if( keys.size() != values.size() ){
+                throw new RuntimeException( "Key bag and Value bag are of different sizes" );
+            }
+            
             Iterator<Tuple> itKeys = keys.iterator();
             Iterator<Tuple> itValue = values.iterator();
             
             while( itKeys.hasNext() && itValue.hasNext() ) {
                 Tuple keyTuple = itKeys.next();
-                Object key = (Object)keyTuple.get(0);
+                String key = (String)keyTuple.get(0);
 
                 Tuple valueTuple = itValue.next();
                 Object value = (Object)valueTuple.get(0);
